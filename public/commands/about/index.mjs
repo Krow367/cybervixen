2635c19567a534d4f8665c090b46c95a4c87e95f
Kat <katrinarwolff@gmail.com>
2026-05-28 19:44:19 -0400

    added about page

---
+++ public/commands/about/index.mjs
@@ -0,0 +1,11 @@
+import { openWindow } from "../../io.js";
+
+const output = [
+    "I have no mouth and I must scream"
+];
+
+export { output };
+export default function () {
+    openWindow("about");
+  return {};
+}
